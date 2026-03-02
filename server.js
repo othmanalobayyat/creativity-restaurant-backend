7fe91aa4f682a641a904df7c4e82dab27880cf5f
othmanalobayyat <othmanalobayyat@gmail.com>
2026-03-02 05:13:55 +0200

    fix upload route for render

--- server.js
+++ server.js
@@ -13,7 +13,10 @@ app.use(cors());
 app.use(express.json({ limit: "15mb" }));
 app.use(express.urlencoded({ extended: true, limit: "15mb" }));
 
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use(
+  "/uploads",
+  express.static(require("path").join(process.cwd(), "uploads")),
+);
 
 const menuRoutes = require("./src/routes/menu.routes");
 const addressRoutes = require("./src/routes/address.routes");
