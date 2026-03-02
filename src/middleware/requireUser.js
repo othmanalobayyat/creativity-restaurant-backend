91b12a59b2cb3883bf4ae8bb8730ecb7d8369e0e
othmanalobayyat <othmanalobayyat@gmail.com>
2026-03-03 01:29:30 +0200

    chore: remove unused legacy requireUser middleware

--- src/middleware/requireUser.js
+++
@@ -1,9 +0,0 @@
-// src/middleware/requireUser.js
-function requireUser(req, res, next) {
-  const userId = Number(req.header("x-user-id") || 0);
-  if (!userId) return res.status(401).json({ error: "Missing x-user-id" });
-  req.userId = userId;
-  next();
-}
-
-module.exports = { requireUser };
